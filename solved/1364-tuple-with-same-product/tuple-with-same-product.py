45fb36c74d84c025a127daf6e59b114fd0da2015
Kevin Thomas <78163578+kevingerard2819@users.noreply.github.com>
2025-02-05 21:26:00 -0800

    Time: 351 ms (67.77%) | Memory: 46.5 MB (22.27%) - LeetSync

---
+++ solved/1364-tuple-with-same-product/tuple-with-same-product.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = defaultdict(int)
+        for i in range(n-1):
+            x=nums[i]
+
+            for j in range(i+1,n):
+                freq[x*nums[j]]+=1
+
+
+        return sum( f*(f-1)*4 for f in freq.values())       
+        
